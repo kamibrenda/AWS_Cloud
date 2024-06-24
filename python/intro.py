dbda05de7e8769fef0bca9fadbdae1638d7e981e
kamibrenda <mukamibrenda254@gmail.com>
2024-06-24 10:11:33 +0300

    python files

---
+++ python/intro.py
@@ -0,0 +1,6 @@
+#function definition 
+def monday_greetings(name):
+    return f"Hello Good Morning {name}, REJECT FINANCE BILL "
+names = ["William", "Joy", "Christine", "Michelle"]
+for name in names:
+    print(monday_greetings(name))
