577f12be051c7431a31b05119f843fe1c408e497
kamibrenda <mukamibrenda254@gmail.com>
2024-07-07 20:55:03 +0300

    caesar-cipher func

---
+++ python/caesar-cipher.py
@@ -0,0 +1,9 @@
+#function to concatenate strings
+def getDoubleAlphabet(alphabet):
+    doubleAlphabet = alphabet + alphabet
+    return doubleAlphabet
+
+#function to encrypt input text
+def getMessage():
+    stringToEncrypt = input("Please enter a message to encrypt:")
+    return stringToEncrypt 
