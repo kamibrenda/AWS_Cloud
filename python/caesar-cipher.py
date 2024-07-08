2a26b988daf98e742d81bef18ec22bc61d224174
kamibrenda <mukamibrenda254@gmail.com>
2024-07-08 14:15:14 +0300

    py file update

--- python/caesar-cipher.py
+++ python/caesar-cipher.py
@@ -7,3 +7,17 @@ def getDoubleAlphabet(alphabet):
 def getMessage():
     stringToEncrypt = input("Please enter a message to encrypt:")
     return stringToEncrypt 
+
+#For encryption an decryption
+def encryptMessage(message, cipherKey, alphabet):
+    encryptedMessage = ""
+    uppercaseMessage = ""
+    uppercaseMessage = message.upper()
+    for currentCharacter in uppercaseMessage:
+        position = alphabet.find(currentCharacter)
+        newPosition = position + int(cipherKey)
+        if currentCharacter in alphabet:
+            encryptedMessage = encryptedMessage + alphabet[newPosition]
+        else:
+            encryptedMessage = encryptedMessage + currentCharacter
+    return encryptedMessage
--- python/test.py
+++ python/test.py
@@ -1,17 +1,12 @@
-preproinsulin_seq = """
-ORIGIN      
-        1 malwmrllpl lallalwgpd paaafvnqhl cgshlvealy lvcgergffy tpktrreaed
-       61 lqvgqvelgg gpgagslqpl alegslqkrg iveqcctsic slyqlenycn
-//
-"""
-
-# Remove unwanted characters
-clean_seq = preproinsulin_seq.replace("ORIGIN", "").replace("//", "")
-clean_seq = "".join(clean_seq.split()).lower()  # Remove spaces and newlines, convert to lowercase
-
-# Write the cleaned sequence to a file
-with open("preproinsulin-seq-clean.txt", "w") as file:
-    file.write(clean_seq)
-
-# Verify the file length
-print(f"Length of preproinsulin-seq-clean.txt: {len(clean_seq)}")  # Should be 110
+def encryptMessage(message, cipherKey, alphabet):
+    encryptedMessage = ""
+    uppercaseMessage = ""
+    uppercaseMessage = message.upper()
+    for currentCharacter in uppercaseMessage:
+        position = alphabet.find(currentCharacter)
+        newPosition = position + int(cipherKey)
+        if currentCharacter in alphabet:
+            encryptedMessage = encryptedMessage + alphabet[newPosition]
+        else:
+            encryptedMessage = encryptedMessage + currentCharacter
+    return encryptedMessage
