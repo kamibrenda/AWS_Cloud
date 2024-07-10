b8854b2036ce3c5cb2c66b598f7093a87bbb5a53
kamibrenda <mukamibrenda254@gmail.com>
2024-07-10 14:54:29 +0300

    scripting exercise

---
+++ python/results.py
@@ -0,0 +1,11 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+for num in range(1, 251):
+    if is_prime(num):
+        print(num, end=' ')
