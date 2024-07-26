c63f06f701479cad7d34176d81425fd9056cbb79
kamibrenda <mukamibrenda254@gmail.com>
2024-07-26 11:00:57 +0300

    aws_services

---
+++ SQL/aws_services.sql
@@ -0,0 +1,31 @@
+--CREATE DATABASE aws_service;
+
+CREATE TABLE `country` (
+`Code` CHAR(3) NOT NULL DEFAULT '',
+`Name` CHAR(52) NOT NULL DEFAULT '',
+`Continent` enum('Asia','Europe','North America','Africa','Oceania','Antarctica','South America') NOT NULL DEFAULT 'Asia',
+`Region` CHAR(26) NOT NULL DEFAULT '',
+`SurfaceArea` FLOAT(10,2) NOT NULL DEFAULT '0.00',
+`IndepYear` SMALLINT(6) DEFAULT NULL,
+`Population` INT(11) NOT NULL DEFAULT '0',
+`LifeExpectancy` FLOAT(3,1) DEFAULT NULL,
+`GNP` FLOAT(10,2) DEFAULT NULL,
+`GNPOld` FLOAT(10,2) DEFAULT NULL,
+`LocalName` CHAR(45) NOT NULL DEFAULT '',
+`GovernmentForm` CHAR(45) NOT NULL DEFAULT '',
+`Capital` INT(11) DEFAULT NULL,
+`Code2` CHAR(2) NOT NULL DEFAULT '',
+PRIMARY KEY (`Code`)
+);
+
+INSERT INTO `country` VALUES ('GAB','Gabon','Africa','Central Africa',267668.00,1960,1226000,50.1,5493.00,5279.00,'Le Gabon','Republic',902,'GA');
+
+INSERT INTO `country` VALUES ('IRL','Ireland','Europe','British Islands',70273.00,1921,3775100,76.8,75921.00,73132.00,'Ireland/Éire','Republic',1447,'IE');
+
+INSERT INTO `country` VALUES ('THA','Thailand','Asia','Southeast Asia',513115.00,1350,61399000,68.6,116416.00,153907.00,'Prathet Thai','Constitutional Monarchy',3320,'TH');
+
+INSERT INTO `country` VALUES ('CRI','Costa Rica','North America','Central America',51100.00,1821,4023000,75.8,10226.00,9757.00,'Costa Rica','Republic',584,'CR');
+
+INSERT INTO `country` VALUES ('AUS','Australia','Oceania','Australia and New Zealand',7741220.00,1901,18886000,79.8,351182.00,392911.00,'Australia','Constitutional Monarchy, Federation',135,'AU');
+
+SELECT * FROM country WHERE GNP > 35000 and Population > 10000000;
