6036689bd1ef3767283dad44170ec84b5c283a3a
kamibrenda <mukamibrenda254@gmail.com>
2024-08-08 16:31:37 +0300

    yaml doc add

---
+++ aws_lambda/lambda_func.py
@@ -0,0 +1,9 @@
+#lambda function before editing 
+import json
+
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
